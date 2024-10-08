b79a229caeeefb45b115f94d1b2c4626b10210cb
KMITCHELLE <mitchelle.okemwa@strathmore.edu>
2024-10-08 18:29:44 +0300

    first commit

--- 144979-Mitchelle-Okemwa/test.cpp
+++ 144979-Mitchelle-Okemwa/test.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main ()
 {
     int number;
-    cout << "Enter a number";
-    cin >> number;
-    cout << "You entered " + number << endl;
+    cout << "Enter a number";//prompt user for input
+    cin >> number; //capture input
+    cout << "You entered " + number << endl; //display 
     cerr<<"This is an example of an error message";
 }
