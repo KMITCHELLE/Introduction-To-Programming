6c504da267a415e83deb81e650987c5797556921
KMITCHELLE <mitchelle.okemwa@strathmore.edu>
2024-10-15 18:48:13 +0300

    status

--- 144979-Mitchelle-Okemwa/helloWorld.cpp
+++ 144979-Mitchelle-Okemwa/helloWorld.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main ()
 {
     cout << "Hello World. Glad to begin learning C++";
+    cout <<"Strathmore University is the leading \n university in the region";
     cout <<"My name is Kemzie";
     return 0;
 }
