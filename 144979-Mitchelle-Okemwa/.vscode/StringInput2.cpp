70f59bc239b36d317f8b355e74761a3b968bf838
KMITCHELLE <mitchelle.okemwa@strathmore.edu>
2024-11-05 20:03:13 +0300

    status

---
+++ 144979-Mitchelle-Okemwa/.vscode/StringInput2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Declare a string variable
+    string myName;
+
+    //Prompt user for their full name
+cout << "Please enter your full name: ";
+cin >> myName;
+
+cout << " Your name is" << myName << endl;
+
+}
