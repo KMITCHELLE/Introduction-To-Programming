4518c07de2a0670e4086ff7ff833664e185626b7
KMITCHELLE <mitchelle.okemwa@strathmore.edu>
2024-11-05 20:24:51 +0300

    status

--- 144979-Mitchelle-Okemwa/.vscode/StringInput2.cpp
+++ 144979-Mitchelle-Okemwa/.vscode/StringInput2.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    //Declare a string variable
-    string myName;
+    // Declare a string variable 
+    string myName, description;
 
-    //Prompt user for their full name
-cout << "Please enter your full name: ";
-cin >> myName;
+// Prompt user for their full name
+ cout << "Please en your full name: ";
+ getline(cin, myName);
+  // Prompt user for their description 
+cout << "Please describe yourself: "; 
+getline(cin, description);
+cout << "Your name is "<< myName << endl;
+cout << "You said the following about yourself: " << description << endl;
 
-cout << " Your name is" << myName << endl;
-
-}
+ }
